91bc8e0f187cf6ce23283ef01fbb47e7b5b86053
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-06-29 09:37:10 -0300

    verificação de aluno

--- AvAliado/adminAvaliado/insertAluno.php
+++ AvAliado/adminAvaliado/insertAluno.php
@@ -14,12 +14,24 @@ $sql_cadastro = "INSERT INTO pessoa(matricula, tipoid, nome, telefone, email) VA
 $sql_usuario = "INSERT INTO usuario(matricula, senha, tipoid) VALUES ('$matricula_aluno', 123, 1)";
 $sql_aluno = "INSERT INTO aluno VALUES ('$matricula_aluno', '$periodo')";
 
-$sql_execute = $mysqli->query($sql_cadastro) or die($mysqli->error);
-$sql_execute = $mysqli->query($sql_usuario) or die($mysqli->error);
-$sql_execute = $mysqli->query($sql_aluno) or die($mysqli->error);
-echo("
+//$sql_execute = $mysqli->query($sql_cadastro) or die($mysqli->error);
+if ($mysqli->query($sql_cadastro)){
+    $sql_execute_user = $mysqli->query($sql_usuario) or die($mysqli->error);
+    $sql_execute_aluno = $mysqli->query($sql_aluno) or die($mysqli->error);
+    echo("
         <script>
             location.href='cadastroaluno.php';
         </script>
-    ")
+    ");
+}else{
+    echo("
+        <script>
+        alert('O usuário ".$idPessoa." já está cadastrado');
+            location.href='cadastroaluno.php';
+        </script>
+
+    ");
+}
+
+
 ?>
