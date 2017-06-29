b1b9b12c532db24cab646417cdc0497ff3d2ca13
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-06-29 11:01:55 -0300

    Alteração para tentar receber o numero de linhas

--- AvAliado/adminAvaliado/insertVinculoAlunoDisciplinaOferta.php
+++ AvAliado/adminAvaliado/insertVinculoAlunoDisciplinaOferta.php
@@ -12,13 +12,21 @@ $semestre = $_POST['semestre'];
 $sql_teste_tem_vinculo = "SELECT * FROM vinculouniversidade WHERE pessoaID='$idAluno' and universidadeID = (SELECT distinct(universidadeid) FROM Disciplinaoferta JOIN Disciplina JOIN Curso where DisciplinaOferta.id = Disciplina.id AND Disciplina.cursoID = curso.id AND DisciplinaOferta.id = '$idDisciplina')";
 
 if($mysqli->query($sql_teste_tem_vinculo)){
-    $sql_insert_vinculo = "INSERT INTO vinculoAlunoDisciplinaOferta VALUES ('$idDisciplina', '$idProfessor', '$ano', '$semestre', '$idAluno')";
-    $sql_exec_insert_vinculo = $mysqli->query($sql_insert_vinculo);
-    echo("
+    $exec = $mysqli->query($sql_teste_tem_vinculo);
+    $num = $exec->num_rows;
+    if ($num > 0){
+        echo "maior que zero";
+    }else{
+        echo ($num);
+        echo "oi";
+    }
+    //$sql_insert_vinculo = "INSERT INTO vinculoAlunoDisciplinaOferta VALUES ('$idDisciplina', '$idProfessor', '$ano', '$semestre', '$idAluno')";
+    //$sql_exec_insert_vinculo = $mysqli->query($sql_insert_vinculo);
+    /*echo("
         <script>
             location.href='cadastrovinculoAlunoDisciplinaOferta.php';
         </script>
-    ");
+    ");*/
 }else{
     echo("
         <script>
