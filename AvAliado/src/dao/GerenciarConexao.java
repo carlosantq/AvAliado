187dab643deef49c490ebf37de1d4d2798cb9bc3
Carlos Antonio <carlosantoniooln@gmail.com>
2017-09-04 22:37:50 -0300

    Update GerenciarConexao.java

--- AvAliado/src/dao/GerenciarConexao.java
+++ AvAliado/src/dao/GerenciarConexao.java
@@ -9,7 +9,7 @@ public class GerenciarConexao {
   public static Connection getConexao() {
 	  if (conexao == null) {  	
 		  String username = "root";
-		  String password = "sip12e12";
+		  String password = "password";
 		  String url = "jdbc:mysql://localhost:3307/AvAliado";
 		  try {
 			  Class.forName("com.mysql.jdbc.Driver");
